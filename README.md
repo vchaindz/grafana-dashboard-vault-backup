bce8b09fb22c6822d47fc61a19fa9b3b85118993
vchaindz <dennis@codenotary.com>
2023-07-10 09:48:09 -0400

    first script

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# grafana-dashboard-vault-backup
+# Grafana Dashboard Backup
+
+This script allows you to back up all Grafana dashboards by sending their data to the immudb Vault service. The dashboards are fetched from a Grafana instance and then each dashboard's data is sent to the immudb Vault service.
+
+## Prerequisites
+
+- Python 3
+- `requests` library in Python
+- Access to a Grafana instance and its API Key
+- Access to an immudb Vault service and its API Key
+
+## Usage
+
+```bash
+python backup_grafana_dashboards.py --url <grafana_url> --api_key <grafana_api_key> --vault_api_key <immudb_vault_api_key>
+```
+
+Replace `<grafana_url>` with the URL of your Grafana instance, `<grafana_api_key>` with your Grafana API Key, and `<immudb_vault_api_key>` with your immudb Vault API Key.
+
+If the immudb Vault API Key is not provided as a command line argument, the script will attempt to read it from the `VAULT_IMMUDB_API_KEY` environment variable.
+
+## Description
+
+The script performs the following steps:
+
+1. It first initializes a collection in the immudb Vault service with appropriate field definitions and indexes.
+2. It then fetches all dashboards from the Grafana instance.
+3. For each dashboard, it sends a request to fetch the detailed dashboard data.
+4. It prepares a record for each dashboard with the 'uid', 'title' and the entire dashboard data.
+5. Each record is then sent to the immudb Vault service.
+
+The script will print the 'uid' and 'title' of each dashboard that it processes. If any request fails, the script will raise an exception and stop.
+
+## Notes
+
+- The script uses the `requests` library to send HTTP requests. Make sure to install this library using pip: `pip install requests`
+- The Grafana API Key needs to have sufficient permissions to read all dashboards.
+- The immudb Vault service needs to accept the API Key and allow the operations that the script performs.
+- The ledger and collection used in the script should be replaced with the actual ledger and collection names for your immudb Vault service.
+- The script does not handle all possible error situations, so it might need adjustments based on your specific use case and environment.
+
---
+++ shipdashboards.py
@@ -0,0 +1,74 @@
+import os
+import requests
+import json
+import argparse
+
+# Parse arguments from command line
+parser = argparse.ArgumentParser(description='Backup Grafana dashboards')
+parser.add_argument('--url', required=True, help='Grafana URL')
+parser.add_argument('--api_key', required=True, help='Grafana API Key')
+parser.add_argument('--vault_api_key', type=str, help='API key for the vault.immudb.io service.')
+args = parser.parse_args()
+
+grafana_url = args.url
+api_key = args.api_key
+
+# Get API key for the vault.immudb.io service from command line argument or environment variable
+vault_api_key = args.vault_api_key or os.getenv('VAULT_IMMUDB_API_KEY', None)
+if not vault_api_key:
+    raise Exception('No API key provided for the vault.immudb.io service.')
+
+# Define headers for requests
+headers = {
+    "Authorization": "Bearer " + api_key,
+    "Accept": "application/json",
+}
+
+# Creating a immudb Vault Session to handle the requests
+s = requests.Session()
+s.headers.update({"Content-Type": "application/json", "X-API-KEY": vault_api_key})
+
+# Define ledger and collection parameters
+ledger = "default" # replace with your ledger
+collection = "default" # replace with your collection
+
+# Initialize the collection within the immudb vault
+payload = {
+    "fields": [{"name": "uid", "type": "INTEGER"}, {"name": "title", "type": "STRING"},{"name": "dashboard", "type": "STRING"}],
+    "idFieldName": "_id",
+    "indexes": [{"fields": ["uid", "title"], "isUnique": True}]
+}
+# ignore for now as the endpoint is currently disabled
+r = s.put(f"https://vault.immudb.io/ics/api/v1/ledger/{ledger}/collection/{collection}", json=payload)
+r.raise_for_status() # If the request failed, this line will raise an exception
+
+
+
+# Get all dashboards
+r = requests.get(grafana_url + '/api/search', headers=headers)
+r.raise_for_status()  # If the request failed, this line will raise an exception
+
+dashboards = r.json()
+
+# Fetch and save each dashboard
+for db in dashboards:
+    r = requests.get(grafana_url + '/api/dashboards/uid/' + db['uid'], headers=headers)
+    r.raise_for_status()
+
+    json_data = r.json()
+    # Remove meta data that's not needed for restore
+    del json_data['meta']
+
+    # Prepare the data for the immudb Vault service
+    record_data = {
+        'uid': db['uid'],
+        'title': json_data['dashboard']['title'],
+        'dashboard': json_data
+    }
+
+    # Send the data to the immudb Vault service
+    inserted = s.put("https://vault.immudb.io/ics/api/v1/ledger/default/collection/default/document", json=record_data)
+    print(db['uid'], json_data['dashboard']['title'])
+    print(inserted.status_code)
+    assert inserted.status_code == 200
+
